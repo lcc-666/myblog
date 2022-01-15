0631c1c3d271b6578d3ae256ae9bc85234808a46
lcc-666 <2502247952@qq.com>
2022-01-15 12:56:57 +0800

    1.15上午项目

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Type,Tags,Blogs
 
 # Register your models here.
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['name','describe']
+
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ['name','describe']
+
+class BlogsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'abstract']
+
+
+admin.site.register(Type,TypeAdmin)
+
+admin.site.register(Tags,TagsAdmin)
+
+admin.site.register(Blogs,BlogsAdmin)
